30fa08c0835f9f3e7b902fc487880fa77c94523f
papu163 <182830006+papu163@users.noreply.github.com>
2025-11-29 14:45:42 -0600

    Add quick setup script for Debian server
    This script sets up a Debian server quickly by configuring network interfaces, DNS, and installing essential packages. It also includes firewall configuration and optional Docker rules.

---
+++ main.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+# Este programa esta hecho para hacer un setup rapido
+# de un servidor en debian, no me hago responsable
+# por posibles errores en el programa que causen 
+# que el sistema pueda ser inestable o directamente
+# romperse
+echo "Asegurate de poner valores validos, de lo contrario podras tener efectos no esperados"
+read -p "Ingresa la direccion IP(privada) que quieres usar: " ipv4_privada
+read -p "Ingresa tu netmask: " netmask
+read -p "Ingresa tu gateway: " gateway
+read -p "Ingresa la DNS que quieres usar: " dns 
+
+#Setup de interfaz y DNS
+echo "Obteniendo interfaz de red principal..."
+INTERFACE=$(ip route show default | awk '{print $5}' | head -n1)
+echo "La interfaz principal es: $INTERFACE"
+echo "Creando copia de seguridad de /etc/network/interfaces en /home"
+dia=$(date)
+hostname=$(hostname -s)
+archivo_cp="$hostname-$dia.txt"
+cp /etc/network/interfaces /home/$archivo_cp
+echo "Copia creada" 
+echo "Aplicando cambios en /etc/network/interfaces"
+echo "" > /etc/network/interfaces
+echo "source /etc/network/interfaces.d/*" >> /etc/network/interfaces
+echo "" >> /etc/network/interfaces
+echo "#loopback" >> /etc/network/interfaces
+echo "auto lo" >> /etc/network/interfaces
+echo "iface lo inet loopback" >> /etc/network/interfaces
+echo "" >> /etc/network/interfaces
+echo "#Primary interface" >> /etc/network/interfaces
+echo "allow-hotplug $INTERFACE" >> /etc/network/interfaces
+echo "iface $INTERFACE inet static" >> /etc/network/interfaces
+echo "" >> /etc/network/interfaces
+echo "address $ipv4_privada" >> /etc/network/interfaces
+echo "netmask $netmask" >> /etc/network/interfaces
+echo "gateway $gateway" >> /etc/network/interfaces
+echo "#ipv6" >> /etc/network/interfaces
+echo "iface $INTERFACE inet6 auto"
+#DNS
+echo "nameserver $dns" > /etc/resolv.conf
+#DNS
+systemctl restart networking.service
+#----Final----
+#Preparacion
+echo -e "\e[32mInstalando paquetes\e[0m: sudo, brightnessctl, ufw"
+apt update && apt upgrade
+apt install sudo brightnessctl ufw -y
+echo "Preparando firewall(ufw)"
+ufw allow ssh
+ufw allow samba
+ufw allow http
+ufw allow https
+ufw enable
+read -p "¿Desea usar ufw-docker(Bloquea a docker de sobrepasar el firewall)?[y/n]: " ufw_docker_create
+if [ "$ufw_docker_create" == "y" || "$ufw_docker_create" == "Y"]; then
+    echo "Creando reglas de ufw-docker..."
+    curl -O https://raw.githubusercontent.com/papu163/debian-server-setup/refs/heads/main/recursos/ufw-docker.txt
+    cat ufw-docker.txt >> /etc/ufw/after.rules
+else
+    echo "Saltando configuracion de ufw-docker"
+fi
+echo "El script ha terminado"
+echo "Aun hay configuracion pendiente, la puedes terminar siguiendo la guia del repositorio de github"
+#----Final----
