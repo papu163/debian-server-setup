fd3c964ce9b8a92ec23a893f6b68783edbba130f
papu163 <182830006+papu163@users.noreply.github.com>
2025-11-29 18:59:42 -0600

    Add Docker installation steps to main.sh
    Added installation steps for Docker and its GPG key.

--- main.sh
+++ main.sh
@@ -60,6 +60,25 @@ if [[ "$ufw_docker_create" == "$yes1" || "$ufw_docker_create" == "$yes" ]]; then
 else
     echo "Saltando configuracion de ufw-docker"
 fi
+echo "Instalando docker..."
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "El script ha terminado"
 echo "Aun hay configuracion pendiente, la puedes terminar siguiendo la guia del repositorio de github"
 #----Final----
